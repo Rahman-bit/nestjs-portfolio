626944250e166e28a7cd1fc403f1917e083e366d
Rahman-bit <rahaman77k@gmail.com>
2024-09-09 16:23:16 +0530

    server url changed

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const server = express();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://rah-portfolio.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization', 
     credentials: true, 
